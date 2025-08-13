1f9a5461b24d449f54b9af70d4718a02d6093726
Pablo Libalde <pablolibalde@gmail.com>
2025-08-13 10:24:38 -0300

    Ainda dando erro

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -15,6 +15,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 from app.db.base import Base  # noqa: E402
+from app.db.models import product, order, offer  # noqa: F401
 target_metadata = Base.metadata
 
 def run_migrations_offline():
